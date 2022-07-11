c5269d563d1e6abf927fbf86c05cdc8cbb2a8e54
Diego Silveira <diegosilveira.dev@gmail.com>
2022-07-11 19:48:52 -0300

    Login - atualização

--- app/src/main/java/br/bruno/projetointegrador/login/ui/FragmentMain.kt
+++ app/src/main/java/br/bruno/projetointegrador/login/ui/FragmentMain.kt
@@ -1,5 +1,6 @@
 package br.bruno.projetointegrador.login.ui
 
 import androidx.fragment.app.Fragment
+import br.bruno.projetointegrador.R
 
-class FragmentMain : Fragment(R.layout.fragment_main)
+class FragmentMain : Fragment(R.layout.tela_login)
